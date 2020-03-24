52238b97877b9912c3810856662eb8ee2076a0c7
bathinapullarao <33596044+bathinapullarao@users.noreply.github.com>
2020-03-24 17:53:37 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ cd CRM
 tar -zcvf myApp_$(date +%Y%d%m)_buildnum_buildno.tar.gz *
 mv myApp_* ../../release
 cd ~/key
-ssh -i id_rsa devops_bpr$webap "sudo mkdir -p /usr/local/myApp"
+ssh -i id_rsa devops_bpr@$webap "sudo mkdir -p /usr/local/myApp"
 ls -l
