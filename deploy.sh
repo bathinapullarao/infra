798a719d042d0d0b4b50f54071ee101a4583bc03
bathinapullarao <33596044+bathinapullarao@users.noreply.github.com>
2020-03-24 15:55:02 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
-#!/bin/bh
+#!/bin/bash
 cd CRM
-tar -zcvf myApp_$(date +%Y%d%m)_buildnum_buildno.tar.gz *
+sudo tar -zcvf myApp_$(date +%Y%d%m)_buildnum_buildno.tar.gz *
+cp -r myApp_* ../../release
