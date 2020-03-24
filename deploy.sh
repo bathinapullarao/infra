269ad8949245b2d0da24c4644a4e680a0fd686a6
bathinapullarao <33596044+bathinapullarao@users.noreply.github.com>
2020-03-24 16:32:19 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,5 @@ webap=$1
 cd CRM
 tar -zcvf myApp_$(date +%Y%d%m)_buildnum_buildno.tar.gz *
 mv myApp_* ../../release
-ssh $webap "sudo mkdir -p /usr/local/myApp"
-ssh $webap "sudo chown -R devops_bpr:devops_bpr /usr/local/myApp"
-ssh $webap "sudo rm -rf /usr/local/myApp/*"
-ssh $webap "sudo mkdir -p /usr/local/backup/"
-ssh $webap "sudo chown -R devops_bpr:devops_bpr /usr/local/backup"
-ssh $webap "sudo rm -rf /usr/local/backup/*"
-scp -r ../../myApp_* $webap:/usr/local/backup/
-ssh $webap "sudo tar -zxvf /usr/local/backup/myApp_* -C /usr/local/"
+cd
+ls -l
