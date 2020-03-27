1ae82bc2c60a29f5fcf586186d51328f1af815e0
bathinapullarao <33596044+bathinapullarao@users.noreply.github.com>
2020-03-27 09:29:13 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 webap=$1
 cd ../dev_application
-tar -zcvf myApp_$(date +%Y%d%m)_buildnum_buildno.tar.gz *
-mv myApp_* ../../release
-
-
+ls -l
+sudo tar -zcvf myApp_$(date +%Y%d%m)_buildnum_buildno.tar.gz *
+sudo chown jenkins:jenkins myApp_*
+sudo mv myApp_* ../../release
